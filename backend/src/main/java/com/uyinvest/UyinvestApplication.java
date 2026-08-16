f08392916c45f79c7e9f69dc11c1a173876c7aca
Emmanuel-Miotti <109709518+Emmanuel-Miotti@users.noreply.github.com>
2026-08-16 15:13:13 -0300

    setup inicial del backend

---
+++ backend/src/main/java/com/uyinvest/UyinvestApplication.java
@@ -0,0 +1,12 @@
+package com.uyinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UyinvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UyinvestApplication.class, args);
+    }
+}
